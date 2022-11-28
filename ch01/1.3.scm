a7715e6b7538ad58e9e38c3a35444de09b0d07aa
krapjost <krapjost@gmail.com>
2022-11-28 19:47:09 +0900

    3

---
+++ ch01/1.3.scm
@@ -0,0 +1,47 @@
+(define plus4 (lambda (x) (+ x 4)))
+
+((lambda (x y z) (+ x y (square z))) 1 2 3)
+
+;; 1.3.3 일반적인 메소드로서의 프로시져
+;;;이분법
+
+(define (average a b)
+  (exact->inexact
+   (/ (+ a b) 2)))
+
+(average 5 10)
+
+(define (search f neg-point pos-point)
+  (let ((midpoint
+         (average neg-point pos-point)))
+    (if (close-enough? neg-point pos-point)
+        midpoint
+        (let ((test-value (f midpoint)))
+          (cond
+           ((positive? test-value)
+            (search f neg-point midpoint))
+           ((negative? test-value)
+            (search f midpoint pos-point))
+           (else midpoint))))))
+
+(define (close-enough? x y)
+  (< (abs (- x y)) 0.001))
+
+(define (half-interval-method f a b)
+  (let ((a-value (f a))
+        (b-value (f b)))
+    (cond ((and (negative? a-value)
+                (positive? b-value))
+           (search f a b))
+          ((and (negative? b-value)
+                (positive? a-value))
+           (search f b a))
+          (else
+           (error "Values are not of
+                   opposite sign" a b)))))
+
+(sin 2.0)
+
+(half-interval-method sin 2.0 4.0)
+
+;;; 고정점
